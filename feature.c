ea60aff7b3e32726a80dd6a99a6159af680e267f
LiuHsin <liuhsin.dev@gmail.com>
2017-07-30 00:35:25 +0900

    Revert "Ready merge to master"

--- feature.c
+++
@@ -1,6 +0,0 @@
-// This is for github collaboration test
-// This is a feature branch
-// LiuHsin add a feature
-// Add more features, and fix some bugs
-// Lochuan add a feature
-// collaborator test
